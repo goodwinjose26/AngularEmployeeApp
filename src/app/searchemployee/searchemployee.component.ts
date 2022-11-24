94c6a6479c6666681e80739b2b5d3f39e49bb058
goodwinjose26 <josegoodwin22@gmail.com>
2022-11-24 16:07:07 +0530

    searchemployee

---
+++ src/app/searchemployee/searchemployee.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-searchemployee',
+  templateUrl: './searchemployee.component.html',
+  styleUrls: ['./searchemployee.component.css']
+})
+export class SearchemployeeComponent {
+
+}
